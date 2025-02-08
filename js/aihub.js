7becc66e70ef62ad53fb2bf7dcfa908f7e114171
Naveen K <101883510+Naveenkumaresan@users.noreply.github.com>
2025-02-08 17:59:43 +0530

    Update aihub.js

--- js/aihub.js
+++ js/aihub.js
@@ -85,7 +85,7 @@ async function generateAnswer() {
           answerOutput.textContent = disclaimer + response;
       } else {
           const response = await axios.post(
-              "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAx_UA7BMYyO128wUK2FsRdQcYb1Ld4A94",
+              "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBsEQlVTEnGh9oEpy8KOA47eA_Oyzl_hbA",
               { contents: [{ parts: [{ text: normalizedQuestion }] }] }
           );
           const aiGeneratedText = response.data.candidates[0].content.parts[0].text;
